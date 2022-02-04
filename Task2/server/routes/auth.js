625ce0d03c8c3dfd4f3829dd8ba52faa31581d69
Kacper Zawojski <zawojweb@gmail.com>
2022-02-04 13:52:28 +0100

    Add server validation

--- Task2/server/routes/auth.js
+++ Task2/server/routes/auth.js
@@ -1,18 +1,32 @@
 const router = require('express').Router()
 const User = require('../model/User')
+const Joi = require('@hapi/joi')
+
+// VALIDATION
+const schemaValidation = Joi.object({
+  name: Joi.string().min(3).required(),
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(6).required(),
+})
 
 router.post('/register', async (req, res) => {
-  const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  })
+  const { error } = schemaValidation.validate(req.body)
 
-  try {
-    const savedUser = await user.save()
-    res.send(savedUser)
-  } catch (error) {
-    res.status(400).send(error)
+  if (error) {
+    return res.status(400).send(error.details[0].message)
+  } else {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+
+    try {
+      const savedUser = await user.save()
+      res.send(savedUser)
+    } catch (error) {
+      res.status(400).send(error)
+    }
   }
 })
 
